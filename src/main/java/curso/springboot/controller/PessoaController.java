f4dc06f7c208328550c8645f14af61c1b2a90325
williamisfranciscodasilva <williamisepaula@gmail.com>
2019-02-20 21:23:47 -0300

    Fazendo a nova tabela receber dados

--- src/main/java/curso/springboot/controller/PessoaController.java
+++ src/main/java/curso/springboot/controller/PessoaController.java
@@ -13,13 +13,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import curso.springboot.model.Pessoa;
+import curso.springboot.model.Telefone;
 import curso.springboot.repository.PessoaRepository;
+import curso.springboot.repository.TelefoneRepository;
 
 @Controller
 public class PessoaController {
 	
 	@Autowired
 	private PessoaRepository pessoaRepository;
+	
+	@Autowired
+	private TelefoneRepository telefoneRepository;
 	
 	@RequestMapping(method=RequestMethod.GET, value="/cadastropessoa")
 	public ModelAndView inicio() {
@@ -86,6 +91,18 @@ public class PessoaController {
 		Optional<Pessoa> pessoa = pessoaRepository.findById(idpessoa);
 		ModelAndView modelAndView = new ModelAndView("cadastro/telefones");
 		modelAndView.addObject("pessoaobj", pessoa.get());
+		return modelAndView;
+	}
+	
+	@PostMapping("**/addfonePessoa/{pessoaid}")
+	public ModelAndView addFonePessoa(Telefone telefone, @PathVariable("pessoaid") Long pessoaid) {
+		
+		Pessoa pessoa = pessoaRepository.findById(pessoaid).get();
+		telefone.setPessoa(pessoa);
+		telefoneRepository.save(telefone);
+		
+		ModelAndView modelAndView = new ModelAndView("cadastro/telefones");
+		modelAndView.addObject("pessoaobj", pessoa);
 		return modelAndView;
 	}
 }
--- src/main/java/curso/springboot/repository/TelefoneRepository.java
+++ src/main/java/curso/springboot/repository/TelefoneRepository.java
@@ -1,5 +1,13 @@
 package curso.springboot.repository;
 
-public interface TelefoneRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import curso.springboot.model.Telefone;
+
+@Repository
+@Transactional
+public interface TelefoneRepository extends CrudRepository<Telefone, Long> {
+	
 }
